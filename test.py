4736ecb29efdf1498138533ec4668f3e6ccf486f
NguyenAnh2003 <cunho2803032003@gmail.com>
2023-07-12 12:07:45 +0700

    #code

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import matplotlib, scipy, sklearn
 import torchcrf
-# print(matplotlib._get_version())
-# print(scipy.__version__)
-# print(sklearn.__version__)
-# print(torchcrf.__version__)
-from predictor.ner_predictor import ner_predictor
+import pydantic
 
-print(ner_predictor("thủ đô Việt Nam là Hà Nội"))
+print(matplotlib._get_version())
+print(scipy.__version__)
+print(sklearn.__version__)
+print(torchcrf.__version__)
+print('pydantic', pydantic.__version__)
