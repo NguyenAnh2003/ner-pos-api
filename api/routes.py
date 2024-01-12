ec37633fba5096e1f91ae2a58eade0e1037a419d
Quocdung <dungttq.21it@vku.udn.vn>
2024-01-12 09:52:33 +0700

    add description for reponse model

--- api/routes.py
+++ api/routes.py
@@ -9,6 +9,10 @@ router = APIRouter()  # fast router
 
 @router.post('/danangnlp/ner', response_model=ResponseNER, status_code=status.HTTP_200_OK)
 def ner_route(request: RequestBody):
+    """
+    :param request: raw sentence
+    :return: NER response model
+    """
     try:
         ner_result = ner_service(request.sentence)
         response_data = ResponseNER(
@@ -24,7 +28,7 @@ def ner_route(request: RequestBody):
 def pos_route(request: RequestBody):
     """
     :param request: raw sentence
-    :return: Pos response model
+    :return: POS response model
     """
     try:
         pos_result = pos_service(request.sentence)
