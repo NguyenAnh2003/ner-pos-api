59197a15256fd2201207793c810e37fb7066f82c
Quocdung <dungttq.21it@vku.udn.vn>
2024-01-12 21:35:58 +0700

    replace raise by JSONreponse in route

--- api/routes.py
+++ api/routes.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter, status, HTTPException, Depends
+from fastapi import APIRouter, status
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi import status
 from api.request_body import RequestBody
 from api.services import *
 from api.response_model import *
 """ defining api router """
 
 router = APIRouter()  # fast router
+
 
 @router.post('/danangnlp/ner', response_model=ResponseNER, status_code=status.HTTP_200_OK)
 def ner_route(request: RequestBody):
@@ -19,8 +23,11 @@ def ner_route(request: RequestBody):
         return response_data
     except Exception as e:
         # Handle other general exceptions
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
+        error_detail = "Internal Server Error: {}".format(str(e))
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={"detail": error_detail}
+        )
 
 
 @router.post('/danangnlp/pos', response_model=ResponsePOS, status_code=status.HTTP_200_OK)
@@ -36,8 +43,11 @@ def pos_route(request: RequestBody):
         return response_data
     except Exception as e:
         # Handle other general exceptions
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
+        error_detail = "Internal Server Error: {}".format(str(e))
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={"detail": error_detail}
+        )
 
 
 @router.post('/danangnlp/wordsegment', status_code=status.HTTP_200_OK)
